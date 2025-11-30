782ad27ab0b2c3a2e963e92bb5c2846fcdfa2f65
4nGi-FES <official4nGi@gmail.com>
2025-11-30 16:36:22 -0500

    E: Added 'get_files_info' and 'get_file_contents'

---
+++ tests.py
@@ -0,0 +1,27 @@
+#tests for get_file_content.py
+
+from functions.get_file_content import get_file_content
+
+def test():
+    result = get_file_content("calculator", "main.py")
+    print("Result for 'main.py' file path:")
+    print(result)
+    print("")
+
+    result = get_file_content("calculator", "pkg/calculator.py")
+    print("Result for 'pkg/calculator.py' file path:")
+    print(result)
+    print("")
+
+    result = get_file_content("calculator", "/bin/cat")
+    print("Result for '/bin/cat' file path:")
+    print(result)
+    print("")
+
+    result = get_file_content("calculator", "pkg//does_not_exist.py")
+    print("Result for 'pkg//does_not_exist.py' file path:")
+    print(result)
+
+
+if __name__ == "__main__":
+    test()
