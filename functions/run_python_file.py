fe2a41bcc63fc63f1d55acf8de577f55b72b1119
4nGi-FES <official4nGi@gmail.com>
2025-12-06 18:37:18 -0500

    G: Added call_function and agent can call the major functions

---
+++ functions/run_python_file.py
@@ -0,0 +1,54 @@
+import os
+import subprocess
+from google.genai import types
+
+schema_run_python_file = types.FunctionDeclaration(
+    name="run_python_file",
+    description="Executes a Python file within the working directory and returns the output from the interpreter.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="Path to the Python file to execute, relative to the working directory.",
+            ),
+            "args": types.Schema(
+                type=types.Type.ARRAY,
+                items=types.Schema(
+                    type=types.Type.STRING,
+                    description="Optional arguments to pass to the Python file.",
+                ),
+                description="Optional arguments to pass to the Python file.",
+            ),
+        },
+        required=["file_path"],
+    ),
+)
+
+
+def run_python_file(working_directory, file_path, args=[]):
+    abs_working_dir = os.path.abspath(working_directory)
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+    if not abs_file_path.startswith(abs_working_dir):
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+    if not os.path.exists(abs_file_path):
+        return f'Error: File "{file_path}" not found.'
+    if not file_path.endswith(".py"):
+        return f'Error: "{file_path}" is not a Python file.'
+    process_list = ["python", abs_file_path]
+    if args:
+        process_list.extend(args)
+    try:
+        result = subprocess.run(process_list, timeout=30, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, cwd=abs_working_dir,)
+        output_parts = []
+        if result.stdout:
+            output_parts.append("STDOUT:\n" + result.stdout)
+        if result.stderr:
+            output_parts.append("STDERR:\n" + result.stderr)
+        if result.returncode != 0:
+            output_parts.append(f"Process exited with code {result.returncode}")
+        if not output_parts:
+            return "No output produced"
+        return "\n".join(output_parts)
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
